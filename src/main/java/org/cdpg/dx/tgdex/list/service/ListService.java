fa32c11cf2a366e0ddc4cf97b7dcf2febbabeb43
Ankit Singh <101859999+ankitmashu@users.noreply.github.com>
2025-07-21 19:18:34 +0530

    dto

--- src/main/java/org/cdpg/dx/tgdex/list/service/ListService.java
+++ src/main/java/org/cdpg/dx/tgdex/list/service/ListService.java
@@ -2,8 +2,9 @@ package org.cdpg.dx.tgdex.list.service;
 
 import io.vertx.core.Future;
 import io.vertx.core.json.JsonObject;
+import org.cdpg.dx.database.elastic.model.QueryDecoderRequestDTO;
 import org.cdpg.dx.tgdex.search.util.ResponseModel;
 
 public interface ListService {
-    Future<ResponseModel> getAvailableFilters(JsonObject jsonObject);
+    Future<ResponseModel> getAvailableFilters(QueryDecoderRequestDTO queryDecoderRequestDTO);
 }
